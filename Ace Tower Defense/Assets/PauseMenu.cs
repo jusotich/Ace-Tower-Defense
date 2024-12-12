923c71019b740a6fddecddec0b54265de7b928f5
Nilskarlsso <142808913+Nilskarlsso@users.noreply.github.com>
2024-12-12 14:32:57 +0100

    somwhat finnised
    typ färdig pågrund av vi inte har någon annan meny

--- Ace Tower Defense/Assets/PauseMenu.cs
+++ Ace Tower Defense/Assets/PauseMenu.cs
@@ -5,10 +5,12 @@ public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
 
+    public GameObject pauseMenuUI;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUpKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (GameIsPaused)
             {
@@ -21,12 +23,27 @@ public class PauseMenu : MonoBehaviour
         } 
     }
 
-    void resume()
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause ()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
     {
 
     }
-    void Pause ()
+
+    public void QuitGame()
     {
-        
+
     }
 }
