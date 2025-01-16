3c894f2eeb4dae99c736c96c0756ebdc1e36a45c
Joachim <joachim.hedlof@elev.ga.lbs.se>
2025-01-16 09:38:19 +0100

    Targeting system v2
    Added some code to the targeting system

--- Ace Tower Defense/Assets/Scripts/TargetingSystem.cs
+++ Ace Tower Defense/Assets/Scripts/TargetingSystem.cs
@@ -5,8 +5,19 @@ using UnityEngine.Rendering;
 
 public class TargetingSystem : MonoBehaviour
 {
+    [Header("Variabler")]
+    [SerializeField] private float range = 5f;
 
-    [SerializeField] private float range;
+    [Header("Referenser")]
+    [SerializeField] private Transform TurretRotationPoint;
+
+    private Transform target = null;
+
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.magenta;
+        Handles.DrawWireDisc(transform.position, transform.forward, range);
+    }
 
     private void Awake()
     {
@@ -15,13 +26,21 @@ public class TargetingSystem : MonoBehaviour
 
     private void Update()
     {
+        if (target == null)
+        {
+            FindTarget();
+        }
+    }
+
+    private void FindTarget()
+    {
         Collider2D[] colliderArray = Physics2D.OverlapCircleAll(transform.position, range);
         foreach (Collider2D collider2D in colliderArray)
         {
-            if(collider2D.TryGetComponent<*ReplaceWithSecipt*>(out *ReplaceWithScript* enemy))
-            {
+            /*if(collider2D.TryGetComponent<ReplaceWithSecipt>(out ReplaceWithScript enemy))
+              {
 
-            }
+              }*/
         }
     }
 
