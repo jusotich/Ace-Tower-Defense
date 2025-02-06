ebd0c5c9131391a20df532ad5d42cbae55a59bc7
Nilskarlsso <142808913+Nilskarlsso@users.noreply.github.com>
2025-02-06 14:11:26 +0100

    färdig
    vet inte om det fungerar men det är färdigt

--- Ace Tower Defense/Assets/Scripts/SettingsMenu.cs
+++ Ace Tower Defense/Assets/Scripts/SettingsMenu.cs
@@ -1,11 +1,71 @@
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
-   public AudioMixer audioMixer;
-   public void SetVolume (float volume)
+    public AudioMixer audioMixer;
+
+    public TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start()
     {
-        audioMixer.SetFloat("volume", volume);
+        
+
+        // Kontrollera om den hittades
+        if (resolutionDropdown == null)
+        {
+            Debug.LogError("resolutionDropdown kunde inte hittas! Kontrollera att Dropdown är en child till Canvas.");
+            return;
+        }
+
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+   public void SetVolume (float volume)
+   {
+       audioMixer.SetFloat("volume", volume);
+   }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen) 
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
