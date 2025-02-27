52bc98f5413da82e5d4fee3287d4928c59ca98ee
JelemyLBS <142796402+JelemyLBS@users.noreply.github.com>
2025-02-27 09:09:50 +0100

    map selection now working

--- Ace Tower Defense/Assets/Scripts/MapMenu.cs
+++ Ace Tower Defense/Assets/Scripts/MapMenu.cs
@@ -5,8 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MapMenu : MonoBehaviour
 {
+
+    public int sceneNum;
+
    public void OpenScene()
    {
-        SceneManager.LoadScene("Map 1");
+        SceneManager.LoadScene(sceneNum);
    }
 }
